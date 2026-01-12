73a8be29d49ee76b1a757d3848d596bf28ee81a8
Istvan Fabian <fabian93915@gmail.com>
2026-01-12 09:50:01 +0100

    Remove unnecessary tailwind class

--- frontend/src/shared/components/Navigation.tsx
+++ frontend/src/shared/components/Navigation.tsx
@@ -78,7 +78,7 @@ export default function Navigation() {
             <div className="ml-4 flex items-center md:ml-6">
               <SignedOut>
                 <SignInButton mode="modal">
-                  <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                  <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                     Sign In
                   </button>
                 </SignInButton>
