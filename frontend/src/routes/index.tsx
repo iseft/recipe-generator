74249d6375aa12e7d46afd4e6656a330c4d82413
Istvan Fabian <fabian93915@gmail.com>
2026-01-12 09:34:02 +0100

    Move routes to routes folder

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import AppShell from "./shared/components/AppShell";
-import { PublicRoutes } from "./routes/PublicRoutes";
-import { ProtectedRoutes } from "./routes/ProtectedRoutes";
-import GenerateRecipePage from "./pages/generate-recipe";
-import NotFoundPage from "./pages/NotFoundPage";
+import AppShell from "../shared/components/AppShell";
+import { PublicRoutes } from "./PublicRoutes";
+import { ProtectedRoutes } from "./ProtectedRoutes";
+import GenerateRecipePage from "../pages/generate-recipe";
+import NotFoundPage from "../pages/NotFoundPage";
 
 export function AppRoutes() {
   return (
